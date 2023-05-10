b5b9ec8489b8a831908c9c26af8fdefc54587f26
Harshit Goyal <65660257+harshitgoyal2@users.noreply.github.com>
2023-05-10 12:25:17 +0530

    Managed by Terraform 7th time

---
+++ sample.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self._insert_recursive(data, self.root)
+
+    def _insert_recursive(self, data, node):
+        if data < node.data:
+            if node.left is None:
+                node.left = Node(data)
+            else:
+                self._insert_recursive(data, node.left)
+        else:
+            if node.right is None:
+                node.right = Node(data)
+            else:
+                self._insert_recursive(data, node.right)
+
+    def search(self, data):
+        return self._search_recursive(data, self.root)
+
+    def _search_recursive(self, data, node):
+        if node is None or node.data == data:
+            return node
+        if data < node.data:
+            return self._search_recursive(data, node.left)
+        return self._search_recursive(data, node.right)
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_recursive(self.root, result)
+        return result
+
+    def _inorder_recursive(self, node, result):
+        if node is not None:
+            self._inorder_recursive(node.left, result)
+            result.append(node.data)
+            self._inorder_recursive(node.right, result)
+
+tree = BinaryTree()
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+tree.insert(4)
+
+print(tree.inorder_traversal())  
+print(tree.search(4))  
